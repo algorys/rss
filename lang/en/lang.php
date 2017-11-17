01171a570e2c3efa5184675d41f70113c5c35be4
Felix Mueller-Donath <j.felix@mueller-donath.de>
2017-11-17 12:16:23 +0100

    Update lang.php

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -5,5 +5,5 @@
 $lang['encoding']   = 'utf-8';
 $lang['direction']  = 'ltr';
 
-$lang['rss.publish'] = 'publish at';
-$lang['rss.empty'] = 'The Feed is empty !';
+$lang['rss.publish'] = 'published';
+$lang['rss.empty'] = 'The Feed is empty!';
